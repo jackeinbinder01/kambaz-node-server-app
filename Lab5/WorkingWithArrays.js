e0dbba5fa9e0fb7b6f8153b661b1f3a0228b767c
Jack Einbinder <jackeinbinder01@gmail.com>
2025-03-27 17:21:01 -0400

    lab 5 completed

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -15,6 +15,11 @@ export default function WorkingWithArrays(app) {
         todos.push(newTodo);
         res.json(todos);
     });
+    app.post("/lab5/todos", (req, res) => {
+        const newTodo = { ...req.body, id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });
     app.get("/lab5/todos", (req, res) => {
         const { completed } = req.query;
         if (completed != undefined) {
@@ -24,7 +29,6 @@ export default function WorkingWithArrays(app) {
             res.json(completedTodos);
             return;
         }
-       
         res.json(todos);
     });
     app.get("/lab5/todos/:id", (req, res) => {
@@ -39,11 +43,18 @@ export default function WorkingWithArrays(app) {
     app.get("/lab5/todos/:id/delete", (req, res) => {
         const { id } = req.params;
         const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
-        if (!todo) {
-            return res.status(404).json({ error: "Todo not found" });
-        }
         todos.splice(todoIndex, 1);
         res.json(todos);
+    });
+    app.delete("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to delete Todo with ID: ${id}` });
+            return;
+        }
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
     });
     // update todo title
     app.get("/lab5/todos/:id/title/:title", (req, res) => {
@@ -74,5 +85,20 @@ export default function WorkingWithArrays(app) {
         }
         todo.completed = completed === "true";
         res.json(todos);
+    });
+    // update todo with PUT
+    app.put("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to update Todo with ID: ${id}` });
+            return;
+        }
+        todos = todos.map((t) => {
+            if (t.id === parseInt(id)) {
+                return { ...t, ...req.body };
+            }
+            return t;
+        });
+        res.sendStatus(200);
     });
 };
